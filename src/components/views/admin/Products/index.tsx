cc858665261bdfc9aaab6ec24a9dcf7f53529f77
syahridho <syahridhosyahputra@gmail.com>
2024-03-09 21:36:43 +0700

    feat: add product 5 menit

--- src/components/views/admin/Products/index.tsx
+++ src/components/views/admin/Products/index.tsx
@@ -1,12 +1,99 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import AdminLayout from "@/components/layouts/AdminLayout";
+import Button from "@/components/ui/Button";
+import { User } from "@/types/user.type";
+import { useSession } from "next-auth/react";
 
-const ProductsAdminView = () => {
+type PropTypes = {
+  users: User[];
+  setToaster: Dispatch<SetStateAction<{}>>;
+};
+
+const ProductsAdminView = (props: PropTypes) => {
+  const { users, setToaster } = props;
+  const session: any = useSession();
+
+  const [updateUser, setUpdateUser] = useState<User | {}>({});
+  const [deleteUser, setDeleteUser] = useState<User | {}>({});
+  const [usersData, setUsersData] = useState<User[]>([]);
+
+  useEffect(() => {
+    setUsersData(users);
+  }, [users]);
+
   return (
-    <AdminLayout>
-      <div>
-        <div>Admin Layout</div>
-      </div>
-    </AdminLayout>
+    <>
+      <AdminLayout>
+        <div className="p-12">
+          <h1 className="text-xl font-semibold">Products Management</h1>
+          <table className="border-collapse border border-slate-500 w-full mt-4">
+            <thead className="bg-gray-300">
+              <tr>
+                <th className="border border-slate-200 py-1 px-2 text-sm">#</th>
+                <th className="border border-slate-200 py-1 px-2 text-sm">
+                  Image
+                </th>
+                <th className="border border-slate-200 py-1 px-2 text-sm">
+                  Name
+                </th>
+                <th className="border border-slate-200 py-1 px-2 text-sm">
+                  Price
+                </th>
+                <th className="border border-slate-200 py-1 px-2 text-sm">
+                  Stock
+                </th>
+                <th className="border border-slate-200 py-1 px-2 text-sm">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersData.map((user: User, index: number) => (
+                <tr key={user.id}>
+                  <td className="border border-slate-200 py-1 px-2 text-sm">
+                    {index + 1}
+                  </td>
+                  <td className="border border-slate-200 py-1 px-2 text-sm">
+                    {user.fullname}
+                  </td>
+                  <td className="border border-slate-200 py-1 px-2 text-sm">
+                    {user.email}
+                  </td>
+                  <td className="border border-slate-200 py-1 px-2 text-sm">
+                    {user.phone}
+                  </td>
+                  <td className="border border-slate-200 py-1 px-2 text-sm">
+                    {user.role}
+                  </td>
+                  <td className="border border-slate-200 py-1 px-2 text-sm">
+                    <div className="flex gap-2">
+                      <Button
+                        type="button"
+                        variant="bg-slate-800 text-white hover:bg-slate-700"
+                        className="px-2"
+                        // onClick={() => setUpdateUser(user)}
+                      >
+                        <i className="bx bxs-edit-alt me-1.5" />
+                        Update
+                      </Button>
+                      <Button
+                        type="button"
+                        variant="bg-red-600 text-white hover:bg-red-700 border-red-600"
+                        className="px-2"
+                        // onClick={() => setDeleteUser(user)}
+                      >
+                        <i className="bx bxs-trash me-1.5" />
+                        Delete
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </AdminLayout>
+    </>
   );
 };
 
--- src/pages/admin/products/index.tsx
+++ src/pages/admin/products/index.tsx
@@ -1,11 +1,21 @@
 import ProductsAdminView from "@/components/views/admin/Products";
+import userServices from "@/services/user";
+import { useEffect, useState } from "react";
 
-const AdminProductPage = () => {
+const AdminProductsPage = ({ setToaster }: any) => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const { data } = await userServices.getAllUser();
+      setUsers(data.data);
+    };
+    getAllUsers();
+  }, []);
   return (
     <>
-      <ProductsAdminView />
+      <ProductsAdminView users={users} setToaster={setToaster} />
     </>
   );
 };
 
-export default AdminProductPage;
+export default AdminProductsPage;
